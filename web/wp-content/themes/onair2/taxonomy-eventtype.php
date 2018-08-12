d85d049f0f2accd1a783c256b802fa10c761b497
Maxime Pierre <max@webmediasolutionz.com>
2018-08-11 21:20:32 -0400

    removing unfinished theme

--- web/wp-content/themes/onair2/taxonomy-eventtype.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*   Package: OnAir2
-*   Description: Event type archive
-*   Version: 0.0.0
-*   Author: QantumThemes
-*   Author URI: http://qantumthemes.com
-**/
-get_template_part('archive','event' ); 
--- web/wp-content/themes/onair2/taxonomy-membertype.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-*   Package: OnAir2
-*   Description: Member category archive
-*   Version: 0.0.0
-*   Author: QantumThemes
-*   Author URI: http://qantumthemes.com
-**/
-get_template_part('archive','members' ); 
