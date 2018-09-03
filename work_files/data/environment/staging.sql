f3e4260f6d7138e2802caa58c8c8d30445fcbb32
Maxime Pierre <max@webmediasolutionz.com>
2018-09-03 14:17:16 -0400

    doing some restructuring

--- work_files/data/environment/staging.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `wp_options` SET `option_value` = 'http://cpam1410.webmediasolutionz.io' WHERE `wp_options`.`option_id` = 3;
-
-UPDATE `wp_options` SET `option_value` = 'http://cpam1410.webmediasolutionz.io' WHERE `wp_options`.`option_id` = 39;
-
-UPDATE `wp_options` SET `option_value` = 'http://cpam1410.webmediasolutionz.io' WHERE `wp_options`.`option_id` = 26191;
